837aec0bcee50b6c1766c0ec319e586f11b364b4
ksb1712 <bharath.sreedhar.k@gmail.com>
2016-01-10 17:52:27 +0530

    Add navigation drawer and tabs

---
+++ app/src/main/java/com/nrcb/app/Fragment1.java
@@ -0,0 +1,22 @@
+package com.nrcb.app;
+
+/**
+ * Created by ksb1712 on 10/1/16.
+ */
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Fragment1 extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.layout_fragment1,null);
+    }
+}
